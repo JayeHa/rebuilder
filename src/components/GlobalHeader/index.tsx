c95c4c191e2061e198d7710f93f2e805019d8271
정원 <jwha94@gmail.com>
2024-03-21 20:24:28 +0900

    style: emotion 추가 및 스타일 초기 설정

--- src/components/GlobalHeader/index.tsx
+++ src/components/GlobalHeader/index.tsx
@@ -1,3 +1,13 @@
+import { css } from "@emotion/react";
+
 export const GlobalHeader = () => {
-  return <header>GlobalHeader</header>;
+  return (
+    <header
+      css={css`
+        background: red;
+      `}
+    >
+      <h1>GlobalHeader</h1>
+    </header>
+  );
 };
