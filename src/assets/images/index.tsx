4a206c151d1db996709174eed0ffe3e49039da93
정원 <jwha94@gmail.com>
2024-03-22 20:19:39 +0900

    chore: svg 파일 추가 (close, menu)

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,2 +1,4 @@
+export { default as Close } from "./close.svg?react";
 export { default as Globe } from "./globe.svg?react";
 export { default as Logo } from "./logo.svg?react";
+export { default as Menu } from "./menu.svg?react";
