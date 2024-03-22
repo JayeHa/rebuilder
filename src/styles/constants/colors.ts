ce32baa7bc72e64eb08c28afaf53cb0dab48278a
정원 <jwha94@gmail.com>
2024-03-22 18:04:16 +0900

    style: colors에 gray3 추가 및 Colors 타입 추가

--- src/styles/constants/colors.ts
+++ src/styles/constants/colors.ts
@@ -2,9 +2,13 @@ export const colors = {
   black: "#000",
   gray1: "#e6e8eb",
   gray2: "#bdc1c7",
+  gray3: "#6f757b",
   background: "#f6f7f8",
   white: "#fff",
   blue1: "#5d6cfa",
   blue2: "#3737cc",
   blue3: "#173cfe",
 };
+
+export type Colors = typeof colors;
+export type ColorKey = keyof Colors;
