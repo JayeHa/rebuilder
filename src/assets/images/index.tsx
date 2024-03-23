f7ccea90f6e92e9bc7dfe275434d67ffa0bc444c
정원 <jwha94@gmail.com>
2024-03-23 23:45:04 +0900

    chore: SolutionSection 미디어 파일 추가

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,5 +1,7 @@
 export { default as BannerLogo } from "./bannerLogo.svg?react";
+export { default as CirclePlay } from "./circlePlay.svg?react";
 export { default as CirclePlus } from "./circlePlus.svg?react";
+export { default as CircleStop } from "./circleStop.svg?react";
 export { default as Close } from "./close.svg?react";
 export { default as Globe } from "./globe.svg?react";
 export { default as Logo } from "./logo.svg?react";
