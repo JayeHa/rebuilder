a5d852837d4dee20484ecfe7b39179119670125c
정원 <jwha94@gmail.com>
2024-03-23 20:42:48 +0900

    feat: useResponsive 훅에 isMobile 추가

--- src/hooks/useResponsive.ts
+++ src/hooks/useResponsive.ts
@@ -6,6 +6,8 @@ type BreakPoint = BreakpointType | "lgDesktop";
 interface UseResponsive {
   breakpoint: BreakPoint;
 
+  isMobile: boolean;
+
   /**
    * 모바일 기기인지 확인하는 속성입니다.
    * 'tablet' 또는 'mobile' 브레이크포인트에 해당하면 true, 그 외에는 false를 반환합니다.
@@ -18,27 +20,33 @@ function getResponsive() {
 
   let breakpoint: BreakPoint = "lgDesktop";
   let isMobileDevice = false;
+  let isMobile = false;
 
   if (innerWidth <= breakpoints.smDesktop) {
     breakpoint = "smDesktop";
     isMobileDevice = false;
+    isMobile = false;
   }
   if (innerWidth <= breakpoints.laptop) {
     breakpoint = "laptop";
     isMobileDevice = false;
+    isMobile = false;
   }
   if (innerWidth <= breakpoints.tablet) {
     breakpoint = "tablet";
     isMobileDevice = true;
+    isMobile = false;
   }
   if (innerWidth <= breakpoints.mobile) {
     breakpoint = "mobile";
     isMobileDevice = true;
+    isMobile = true;
   }
 
   return {
     breakpoint,
     isMobileDevice,
+    isMobile,
   };
 }
 
